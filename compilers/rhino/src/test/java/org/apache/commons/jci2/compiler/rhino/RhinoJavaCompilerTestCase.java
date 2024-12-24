f77fbfe88f5fc747f3f12cb7e1d3f23032795423
MalikUsama27 <Usamamalik7008@gmail.com>
2024-12-24 19:50:07 +0500

    2 blockers solve

--- compilers/rhino/src/test/java/org/apache/commons/jci2/compiler/rhino/RhinoJavaCompilerTestCase.java
+++ compilers/rhino/src/test/java/org/apache/commons/jci2/compiler/rhino/RhinoJavaCompilerTestCase.java
@@ -25,6 +25,7 @@ import org.apache.commons.jci2.core.compiler.JavaCompiler;
 import org.apache.commons.jci2.core.compilers.AbstractCompilerTestCase;
 import org.apache.commons.jci2.core.readers.ResourceReader;
 import org.apache.commons.jci2.core.stores.MemoryResourceStore;
+import org.junit.Test;
 
 /**
  *
@@ -91,53 +92,105 @@ public final class RhinoJavaCompilerTestCase extends AbstractCompilerTestCase {
     }
 
     @Override
-    public void testUppercasePackageNameCompile() throws Exception {
+    @Test
+    public void testCrossReferenceCompilation() throws Exception {
         final JavaCompiler compiler = createJavaCompiler();
 
         final ResourceReader reader = new ResourceReader() {
             final private Map<String, byte[]> sources = new HashMap<String, byte[]>() {
                 private static final long serialVersionUID = 1L;
                 {
-                    put("Jci/Simple.js", (
-                            " var i = 0;\n" +
-                            "\n"
+                    put("jci2/First.js", (
+                            "function First() {\n" +
+                            "    this.callSecond = function() {\n" +
+                            "        return new Second().getMessage();\n" +
+                            "    }\n" +
+                            "}\n"
+                    ).getBytes());
+                    
+                    put("jci2/Second.js", (
+                            "function Second() {\n" +
+                            "    this.getMessage = function() {\n" +
+                            "        return 'Hello from Second';\n" +
+                            "    }\n" +
+                            "}\n"
                     ).getBytes());
                 }};
 
             @Override
-            public byte[] getBytes( final String pResourceName ) {
+            public byte[] getBytes(final String pResourceName) {
                 return sources.get(pResourceName);
             }
 
             @Override
-            public boolean isAvailable( final String pResourceName ) {
+            public boolean isAvailable(final String pResourceName) {
                 return sources.containsKey(pResourceName);
             }
-
+            
         };
 
         final MemoryResourceStore store = new MemoryResourceStore();
         final CompilationResult result = compiler.compile(
                 new String[] {
-                        "Jci/Simple.js"
+                        "jci2/First.js",
+                        "jci2/Second.js"
                 }, reader, store);
 
-        assertEquals(toString(result.getErrors()), 0, result.getErrors().length);
-        assertEquals(toString(result.getWarnings()), 0, result.getWarnings().length);
+        // Assert compilation success
+        assertEquals("Should have no compilation errors", 0, result.getErrors().length);
+        assertEquals("Should have no compilation warnings", 0, result.getWarnings().length);
 
-        final byte[] clazzBytes = store.read("Jci/Simple.class");
-        assertNotNull(clazzBytes);
-        assertTrue(clazzBytes.length > 0);
+        // Assert both class files were generated
+        byte[] firstClassBytes = store.read("jci2/First.class");
+        byte[] secondClassBytes = store.read("jci2/Second.class");
+        
+        assertNotNull("First.class should be generated", firstClassBytes);
+        assertNotNull("Second.class should be generated", secondClassBytes);
+        assertTrue("First.class should not be empty", firstClassBytes.length > 0);
+        assertTrue("Second.class should not be empty", secondClassBytes.length > 0);
     }
 
     @Override
-    public void testCrossReferenceCompilation() throws Exception {
-        // NA
-    }
-
-    @Override
+    @Test
     public void testAdditionalTopLevelClassCompile() throws Exception {
-        // NA
-    }
+        final JavaCompiler compiler = createJavaCompiler();
 
+        final ResourceReader reader = new ResourceReader() {
+            final private Map<String, byte[]> sources = new HashMap<String, byte[]>() {
+                private static final long serialVersionUID = 1L;
+                {
+                    put("jci2/Main.js", (
+                            "function Main() {}\n" +
+                            "function Additional() {}\n"
+                    ).getBytes());
+                }};
+
+            @Override
+            public byte[] getBytes(final String pResourceName) {
+                return sources.get(pResourceName);
+            }
+
+            @Override
+            public boolean isAvailable(final String pResourceName) {
+                return sources.containsKey(pResourceName);
+            }
+        };
+
+        final MemoryResourceStore store = new MemoryResourceStore();
+        final CompilationResult result = compiler.compile(
+                new String[] {"jci2/Main.js"}, reader, store);
+
+        // Assert compilation success
+        assertEquals("Should have no compilation errors", 0, result.getErrors().length);
+        assertEquals("Should have no compilation warnings", 0, result.getWarnings().length);
+
+        // Assert both classes were generated
+        byte[] mainClassBytes = store.read("jci2/Main.class");
+        byte[] additionalClassBytes = store.read("jci2/Additional.class");
+        
+        assertNotNull("Main.class should be generated", mainClassBytes);
+        assertNotNull("Additional.class should be generated", additionalClassBytes);
+        assertTrue("Main.class should not be empty", mainClassBytes.length > 0);
+        assertTrue("Additional.class should not be empty", additionalClassBytes.length > 0);
+    }
 }
