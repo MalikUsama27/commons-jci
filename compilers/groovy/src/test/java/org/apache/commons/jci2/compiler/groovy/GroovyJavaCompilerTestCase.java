59cb9fd7e21b0bcb1772fc4496dcf3297a612978
MalikUsama27 <Usamamalik7008@gmail.com>
2024-12-24 20:28:36 +0500

    workflow done

--- compilers/groovy/src/test/java/org/apache/commons/jci2/compiler/groovy/GroovyJavaCompilerTestCase.java
+++ compilers/groovy/src/test/java/org/apache/commons/jci2/compiler/groovy/GroovyJavaCompilerTestCase.java
@@ -15,122 +15,35 @@
  * limitations under the License.
  */
 
- package org.apache.commons.jci2.compiler.groovy;
+package org.apache.commons.jci2.compiler.groovy;
 
- import java.lang.reflect.Method;
- 
- import org.apache.commons.jci2.core.compiler.JavaCompiler;
- import org.apache.commons.jci2.core.compilers.AbstractCompilerTestCase;
- import org.junit.Test;
- 
- /**
-  * Test case for the GroovyJavaCompiler class.
-  * @author tcurdt
-  */
- public final class GroovyJavaCompilerTestCase extends AbstractCompilerTestCase {
- 
-     @Override
-     public String getCompilerName() {
-         return "groovy";
-     }
- 
-     @Override
-     public JavaCompiler createJavaCompiler() {
-         return new GroovyJavaCompiler();
-     }
- 
-     @Test
-     public void testBasicGroovyCompilation() throws Exception {
-         final String sourceContent = 
-             "class SimpleGroovyClass {\n" +
-             "    String getMessage() {\n" +
-             "        return 'Hello from Groovy'\n" +
-             "    }\n" +
-             "}\n";
-         
-         final boolean success = compileSource(sourceContent);
-         assertTrue("Basic Groovy compilation should succeed", success);
-         
+import org.apache.commons.jci2.core.compiler.JavaCompiler;
+import org.apache.commons.jci2.core.compilers.AbstractCompilerTestCase;
 
-         // Load and verify the compiled class
-         Class<?> compiledClass = loadClass("SimpleGroovyClass");
-         Object instance = compiledClass.getDeclaredConstructor().newInstance();
-         String message = (String) compiledClass.getMethod("getMessage").invoke(instance);
-         assertEquals("Hello from Groovy", message);
+/**
+ *
+ * @author tcurdt
+ */
+public final class GroovyJavaCompilerTestCase extends AbstractCompilerTestCase {
 
-     }
- 
-     private Class<?> loadClass(String className) throws ClassNotFoundException {
-         if (className == null || className.isEmpty()) {
-             throw new ClassNotFoundException("Class name cannot be null or empty");
-         }
-         return Class.forName(className);
-     }
-     
-     private boolean compileSource(String sourceContent) {
-         if (sourceContent == null || sourceContent.isEmpty()) {
-             return false;
-         }
-         try {
-             // Placeholder for actual compilation logic
-             return true;
-         } catch (RuntimeException e) {
-             // Log the exception for debugging purposes
-             System.err.println("Compilation failed: " + e.getMessage());
-             return false;
-         }
-     }
- 
-     @Override
-     @Test
-     public void testInternalClassCompile() throws Exception {
-         final String sourceContent = 
-             "class OuterClass {\n" +
-             "    class InnerClass {\n" +
-             "        String getMessage() {\n" +
-             "            return 'Hello from Inner Class'\n" +
-             "        }\n" +
-             "    }\n" +
-             "}\n";
-         
-         boolean success = compileSource(sourceContent);
-         // Inner classes are not supported in Groovy, so we expect compilation to fail
-         assertFalse("Inner class compilation should fail in Groovy", success);
-     }
- 
-     @Override
-     @Test
-     public void testCrossReferenceCompilation() throws Exception {
-         final String sourceContent = 
-             "import static java.lang.Math.* \n" +
-             "class MathUser {\n" +
-             "    double getPI() {\n" +
-             "        return PI\n" +
-             "    }\n" +
-             "}\n";
-         
+    @Override
+    public String getCompilerName() {
+        return "groovy";
+    }
 
-         boolean success = compileSource(sourceContent);
-         
-         if (success) {
-             try {
-                 Class<?> compiledClass = loadClass("MathUser");
-                 assertNotNull("Compiled class should not be null", compiledClass);
-                 
-                 Object instance = compiledClass.getDeclaredConstructor().newInstance();
-                 assertNotNull("Class instance should not be null", instance);
-                 
-                 Method piMethod = compiledClass.getMethod("getPI");
-                 assertNotNull("getPI method should exist", piMethod);
-                 
-                 double piValue = (Double) piMethod.invoke(instance);
-                 assertEquals("PI value should match Math.PI", Math.PI, piValue, 0.0001);
-             } catch (Exception e) {
-                 fail("Failed to execute compiled class: " + e.getMessage());
-             }
-         } else {
-             // Static imports might not be supported, this is acceptable
-             System.out.println("Static import compilation failed as expected");
-         }
-     }
- }
+    @Override
+    public JavaCompiler createJavaCompiler() {
+        return new GroovyJavaCompiler();
+    }
+
+    @Override
+    public void testInternalClassCompile() throws Exception {
+        // FIXME: inner classes not supported in groovy?
+    }
+
+	@Override
+    public void testCrossReferenceCompilation() throws Exception {
+		// FIXME: static imports not supported in groovy?
+	}
+
+}
